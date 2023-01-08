5503ed7bdeacfaa77c7896314d4d77284ceec25a
neau <davidneau49@gmail.com>
2023-01-08 14:19:00 +0100

    init

---
+++ pokemon-front/src/components/AppNavigation.vue
@@ -0,0 +1,29 @@
+<template>
+  <span>
+      <v-navigation-drawer app v-model="drawer" dark style="border: solid 2px">
+          <v-list>
+              <v-list-item link to='/'>
+                  Home
+              </v-list-item>
+              <v-list-item link to='/about'>
+                  About
+              </v-list-item>    
+          </v-list>
+      </v-navigation-drawer>
+
+      <v-app-bar app color="blue darken-4" dark>
+          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      </v-app-bar>    
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'AppNavigation',
+  data(){
+      return {
+          drawer: false,
+      }
+  }
+}
+</script>
