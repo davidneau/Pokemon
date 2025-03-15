e703a10f60fbc50b5a944121496e04f4b93e47c6
david <davidneau49@gmail.com>
2025-03-15 14:01:50 +0100

    rm music

--- Front/src/router/index.js
+++ Front/src/router/index.js
@@ -3,7 +3,6 @@ import CalendarHome from '../views/Home.vue'
 import StatistiquesPokemon from '../views/Statistiques.vue'
 import PokemonSD from '../views/pokemonSD.vue'
 import Pokedex from '../views/pokedex.vue'
-import Music from '@/views/Music.vue'
 
 const routes = [
   {
@@ -25,11 +24,6 @@ const routes = [
     path: '/pokedex',
     name: 'pokedex',
     component: Pokedex
-  },
-  {
-    path: '/music',
-    name: 'Music',
-    component: Music
   }
 ]
 
