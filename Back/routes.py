29b1d6a52803c8a827eaffc6cf796cb4049166ce
david <davidneau49@gmail.com>
2025-12-15 23:52:30 +0100

    message troll

--- Back/routes.py
+++ Back/routes.py
@@ -144,7 +144,7 @@ def update(request: Request):
     request.app.conn.commit()
     return "OK"
 
-@router.get("/message", methods=["POST"])
+@router.post("/message")
 def receive_message():
     data = request.get_json()
     message = data.get("message")
